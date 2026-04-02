6e4595ed2e1a1c5bf44bb5cb7f4e4540b02811e2
nanbei45 <86958167+nanbei45@users.noreply.github.com>
2026-04-02 15:35:27 +0800

    Update mobileOG.py

--- mobileOG.py
+++ mobileOG.py
@@ -41,12 +41,11 @@ def run_mobileOG(mobileOG_file,diamond_file,mobileOG_output_dir,mobileOG_db_csv)
     ],check=True)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='使用ISEScan检测插入序列')
+    parser = argparse.ArgumentParser(description='')
     parser.add_argument('--input_genomic_file', type=str, required=True)
     parser.add_argument('--diamond_mobileOG_db', type=str, required=True)
     parser.add_argument('--output_dir', type=str, required=True)
 
-    # 执行参数解析
     args = parser.parse_args()
 
     input_genomic_file = args.input_genomic_file
@@ -54,15 +53,10 @@ if __name__ == '__main__':
     output_dir.mkdir(parents=True, exist_ok=True)
     diamond_mobileOG_db = args.diamond_mobileOG_db
 
-    #mobileOG_file = '/data/zqwangyansu/hl/software/mobileOG/mobileOG-db/mobileOG-pl/mobileOGs-pl-kyanite.py'
-    #mobileOG_db_csv = '/data/zqwangyansu/hl/database/mobileOG-db/mobileOG-db-beatrix-1.6-All.csv'
-
-
     prodigal_file = output_dir / "prodigal.faa"
     diamond_file = output_dir / "diamond.txt"
-    #mobileOG_output_file = output_dir / "mobileOG"
 
     run_prodigal(input_genomic_file, prodigal_file)
     run_diamond(prodigal_file,diamond_mobileOG_db,diamond_file)
-    #run_mobileOG(mobileOG_file,diamond_file,mobileOG_output_file,mobileOG_db_csv)
+
 
